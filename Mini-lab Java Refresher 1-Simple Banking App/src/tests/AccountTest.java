6a81a01a0d71989c50f9031a7a9d2b9f3b8b965d
EmmettM785 <159438781+EmmettM785@users.noreply.github.com>
2024-02-13 22:06:13 +0000

    Add files via upload
    Lab 1 added to repository

---
+++ Mini-lab Java Refresher 1-Simple Banking App/src/tests/AccountTest.java
@@ -0,0 +1,22 @@
+package tests;
+
+import model.Account;
+import java.util.Calendar;
+import java.util.Date;
+
+public class AccountTest {
+
+	public static void testConstructor() {
+		
+	}
+	public static void testSetters() {
+		
+	}
+	
+	public static void main(String[] args) {
+		testConstructor();
+		testSetters();
+	}
+	
+	
+}
